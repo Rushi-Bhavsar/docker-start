1a9196f50bdb98b591455a3307fef27e674515d8
Rushikesh Bhavsar <rushi.bhavsar.57@gmail.com>
2023-12-05 13:01:16 +0530

    Checking Deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Redeployed on 5 December</p>
       </div>
     );
   }
