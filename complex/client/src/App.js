4df9cdc082b6d80eb4b1493c5675c6784641ac85
Rushikesh Bhavsar <rushi.bhavsar.57@gmail.com>
2024-01-31 16:18:54 +0530

    code changes

--- complex/client/src/App.js
+++ complex/client/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <div className="App">
         <header>
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React Kubernetes !!!!!!!!</h1>
           <Link to="/">Home</Link>
           <br></br>
           <Link to="/otherpage">Other Page</Link>
